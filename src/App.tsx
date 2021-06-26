eeba93926d00967264eaa778635a597370495d68
antonnur <antonnur89@yandex.ru>
2021-06-26 16:52:13 +0500

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TaskType, Todolist} from "./Todolist";
 
 function App() {
+
+  let tasks1: Array<TaskType> = [
+    {id: 1, title: 'CSS', isDone: true},
+    {id: 2, title: 'JS', isDone: false},
+    {id: 3, title: 'React', isDone: true}
+  ]
+
+  let tasks2: Array<TaskType> = [
+    {id: 1, title: 'Hi', isDone: true},
+    {id: 2, title: 'How are you', isDone: true},
+    {id: 3, title: 'Yo', isDone: false}
+  ]
+
   return (
     <div>
-      <Todolist title={'What to learn'}/>
-      <Todolist title={'Songs'}/>
-      <Todolist title={'Books'}/>
+      <Todolist title={'What to learn'} tasks={tasks1}/>
+      <Todolist title={'Songs'} tasks={tasks2}/>
     </div>
   );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 
-export function Todolist(props: any) {
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+type PropsType = {
+  title: string
+  tasks: Array<TaskType>
+}
+
+export function Todolist(props: PropsType) {
   return (
     <div className="App">
       <div>
@@ -10,9 +21,9 @@ export function Todolist(props: any) {
           <button>+</button>
         </div>
         <ul>
-          <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-          <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-          <li><input type="checkbox" checked={false}/> <span>React</span></li>
+          <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+          <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+          <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
         </ul>
         <div>
           <button>All</button>
@@ -21,6 +32,5 @@ export function Todolist(props: any) {
         </div>
       </div>
     </div>
-
   );
 }
