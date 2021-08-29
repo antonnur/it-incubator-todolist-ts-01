3ab97889aa3ad572b2d9a54ba25168c179269467
Tont <yamalauto1@yandex.ru>
2021-08-29 21:58:00 +0500

    PC Home TodoList 05 (47min)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import './App.css';
 import {TodoList} from "./TodoList";
 import {v1} from 'uuid';
+import {Simulate} from "react-dom/test-utils";
+import keyDown = Simulate.keyDown;
+
+export type FilterValueType = 'all' | 'active' | 'completed'
 
 export type TaskType = {
   id: string
@@ -9,35 +13,52 @@ export type TaskType = {
   isDone: boolean
 }
 
-export type FilterValueType = 'all' | 'active' | 'completed'
+type TodoListType = {
+  id: string
+  title: string
+  filter: FilterValueType
+}
+
+type TasksStateType = {
+  [key: string]: TaskType[]
+}
 
 const App = () => {
-  const [tasks, setTasks] = useState<Array<TaskType>>([
-    {id: v1(), title: 'CSS', isDone: true},
-    {id: v1(), title: 'JS', isDone: false},
-    {id: v1(), title: 'React', isDone: true},
-    {id: v1(), title: 'Hi', isDone: true},
-    {id: v1(), title: 'How are you', isDone: true},
-    {id: v1(), title: 'Yo', isDone: false}
+  // new BLL:
+  const todoListId_1 = v1()
+  const todoListId_2 = v1()
+  const [todoLists, setTodoLists] = useState<TodoListType[]>([
+    {id: todoListId_1, title: ' What to learn', filter: 'all'},
+    {id: todoListId_2, title: ' What to buy', filter: 'all'},
   ])
 
-  const removeTask = (taskId: string) => {
-    // const otherArr = tasks.filter(t => t.id !== taskId)
-    // setTasks(otherArr)
-    setTasks(tasks.filter(t => t.id !== taskId))
+  const [tasks, setTasks] = useState<TasksStateType>({
+    [todoListId_1]: [
+      {id: v1(), title: 'CSS', isDone: true},
+      {id: v1(), title: 'JS', isDone: false},
+      {id: v1(), title: 'React', isDone: true},
+    ],
+    [todoListId_2]: [
+      {id: v1(), title: 'Hi', isDone: true},
+      {id: v1(), title: 'How are you', isDone: true},
+      {id: v1(), title: 'Yo Fish', isDone: false}
+    ]
+  })
+
+  const removeTask = (taskId: string, todoListId: string) => {
+    setTasks({
+      ...tasks,
+      [todoListId]: tasks[todoListId].filter(t => t.id !== taskId)
+    })
   }
 
-  const addTask = (title: string) => {
+  const addTask = (title: string, todoListId: string) => {
     const newTask: TaskType = {id: v1(), title, isDone: false}
-    const copyTask = [...tasks]
-    copyTask.unshift(newTask)
-    setTasks(copyTask)
-
-    // setTasks([...tasks, newTask])
-    // setTasks([{id: 1(), title: title, isDone:false} ,...tasks])
+    tasks[todoListId] = [...tasks[todoListId], newTask]
+    setTasks({...tasks})
   }
 
-  const changeStatus = (taskId: string, isDone: boolean) => {
+  const changeTaskStatus = (taskId: string, isDone: boolean, todoListId: string) => {
     let task = tasks.find(t => t.id === taskId)
     if (task) {
       task.isDone = isDone
@@ -45,10 +66,7 @@ const App = () => {
     setTasks([...tasks])
   }
 
-  const [filter, setFilter] = useState<FilterValueType>('all')
-  const changeFilter = (nextFilter: FilterValueType) => {
-    setFilter(nextFilter)
-  }
+  const changeFilter = (nextFilter: FilterValueType, todoListId: string) => { setFilter(nextFilter) }
 
   //вариант учебный
   let tasksForRender = tasks
@@ -78,7 +96,7 @@ const App = () => {
         removeTask={removeTask}
         changeFilter={changeFilter}
         addTask={addTask}
-        changeStatus={changeStatus}
+        changeStatus={changeTaskStatus}
         filter={filter}
       />
     </div>
