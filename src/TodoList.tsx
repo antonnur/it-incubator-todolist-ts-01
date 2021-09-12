3bb3596eb7ce6335131e8c7cccffc589083e73be
Tont <yamalauto1@yandex.ru>
2021-09-12 21:18:21 +0500

    PC Home TodoList 05 (End)

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -27,10 +27,10 @@ export const TodoList = (props: TodoListPropsType) => {
     }
   }
 
-  const tasksJsxElements = (t: TaskType) => {
+  const getTasksJsxElements = (t: TaskType) => {
     //*выносим переменную из button
     const removeTask = () => props.removeTask(t.id, props.id)
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
       props.changeTaskStatus(t.id, e.currentTarget.checked, props.id)
     }
     return (
@@ -39,8 +39,8 @@ export const TodoList = (props: TodoListPropsType) => {
       >
         <input
           type="checkbox"
-          onChange={onChangeHandler}
           checked={t.isDone}
+          onChange={changeTaskStatus}
         />
         <span>{t.title}</span>
         <button onClick={removeTask}>x</button>
@@ -48,27 +48,32 @@ export const TodoList = (props: TodoListPropsType) => {
       </li>
     )
   }
+  const tasksJsxElements = props.tasks.map(getTasksJsxElements)
 
   //выносим button функции в отдельные переменные
-  const onAllClickHandler = () => props.changeFilter('all', props.id)
-  const onActiveClickHandler = () => props.changeFilter('active', props.id)
-  const onCompletedClickHandler = () => props.changeFilter('completed', props.id)
+  const setAllFilterValue = () => props.changeFilter('all', props.id)
+  const setActiveFilterValue = () => props.changeFilter('active', props.id)
+  const setCompletedFilterValue = () => props.changeFilter('completed', props.id)
   const onTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
-  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+  const onKeyPressAddTask = (e: KeyboardEvent<HTMLInputElement>) => {
     setError(null)
     if (e.key === 'Enter' || e.ctrlKey) addTask()
   }
+  const removeTodoList = () => props.removeTodoList(props.id)
 
+  // JSX
   return (
     <div className="App">
       <div>
-        <h3>{props.title}</h3>
+        <h3>{props.title}
+          <button onClick={removeTodoList}>x</button>
+        </h3>
         <div>
           <input
             value={title}
             onChange={onTitleChangeHandler}
             //добавление task по Enter
-            onKeyPress={onKeyPressHandler}
+            onKeyPress={onKeyPressAddTask}
             className={error ? 'error' : ''}
           />
           <button onClick={addTask}>+</button>
@@ -78,13 +83,13 @@ export const TodoList = (props: TodoListPropsType) => {
           {tasksJsxElements}
         </ul>
         <div>
-          <button onClick={onAllClickHandler}
+          <button onClick={setAllFilterValue}
                   className={props.filter === 'all' ? 'active-filter' : ''}>All
           </button>
-          <button onClick={onActiveClickHandler}
+          <button onClick={setActiveFilterValue}
                   className={props.filter === 'active' ? 'active-filter' : ''}>Active
           </button>
-          <button onClick={onCompletedClickHandler}
+          <button onClick={setCompletedFilterValue}
                   className={props.filter === 'completed' ? 'active-filter' : ''}>Completed
           </button>
         </div>
